4cd231035200a7d9a247bf0a21880635a9a51021
Dmytro Nikitin <nikitin1409@gmail.com>
2016-12-28 14:05:15 +0200

    Js

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+// Namber
+var myNamber = 5;
+// String
+var str = 'text';
+
+// Boolean
+var flag = true;
+var flag2 = false;
+
+// null
+var nullVar = null;
+
+// undefined
+var notDefinedVar = undefined;
+
+var type = typeof(str);
+
+alert(type);
